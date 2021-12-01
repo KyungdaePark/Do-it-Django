0a1e1aa48c01032eaa06715e59abbf8b47a9ea2a
KyungdaePark <imdongle123@gmail.com>
2021-12-01 23:58:28 +0900

    2-10 폼 하는중

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 #from django.http import HttpResponse
 from .models import Question, Answer
+from .forms import QuestionForm
 # Create your views here.
 
 def index(request):
@@ -36,3 +37,23 @@ def answer_create(request, question_id):
     #     create_date = timezone.now()    
     # ) !!!!same!!!!
     return redirect('pybo:detail', question_id = question.id)
+
+def question_create(request):
+    ## 질문 등록하기 버튼 과 저장하기 버튼 둘다 question_create를 호출함 : 구분을 post와 get으로 한다.
+    """
+        pybo 질문등록 : 질문등록 함수를 호출할 때 '저장하기'버튼 : post
+        '질문 등록하기' 버튼 : get 
+    """
+    #post
+    
+    if request.method == 'POST':
+        form = QuestionForm(request.POST) #request.POST 에는 subject와 content가 들어있음
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm() #get
+    context = {'form' : form}
+    return render(request, 'pybo/question_form.html', context)
